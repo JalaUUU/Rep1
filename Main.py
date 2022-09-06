a3e9d5e3a4c5b21e4d742c823f55cbb6f6f131e2
JalaUUU <112912266+JalaUUU@users.noreply.github.com>
2022-09-06 11:34:18 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import sys
-print(int(sys.argv[1])+int(sys.argv[2]))
+print("Сложение - " + str(int(sys.argv[1])+int(sys.argv[2])))
 print("Вычитание - " + str(int(sys.argv[1])-int(sys.argv[2])))
-print(int(sys.argv[1])*int(sys.argv[2]))
-print(int(sys.argv[1])/int(sys.argv[2]))
+print("Умножение - " + str(int(sys.argv[1])*int(sys.argv[2])))
+print("Деление - " + str(int(sys.argv[1])/int(sys.argv[2])))
