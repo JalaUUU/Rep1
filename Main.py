c777de0cd00c66f4dc51959da6ff4e6a99d3b432
JalaUUU <112912266+JalaUUU@users.noreply.github.com>
2022-09-06 11:32:47 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import sys
 print(int(sys.argv[1])+int(sys.argv[2]))
-print("Вычитание - " + int(sys.argv[1])-int(sys.argv[2]))
+print("Вычитание - " + str(int(sys.argv[1])-int(sys.argv[2])))
 print(int(sys.argv[1])*int(sys.argv[2]))
 print(int(sys.argv[1])/int(sys.argv[2]))
